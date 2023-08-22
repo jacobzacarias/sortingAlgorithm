f58c3b64924243566b23cc4bb783119b8da0ca6b
Jacob Zacarias <101657772+jacobzacarias@users.noreply.github.com>
2023-08-22 14:24:40 -0500

    Update functions.cpp
    In this feature, we will calculate the square of each integer and then calculate the square root of each squared value, calculate the square root of each integer in the array and sort them in descending order based on their square root values, and calculate the square of each integer in the array and sort them in ascending order based on their squared values.

--- functions.cpp
+++ functions.cpp
@@ -1,5 +1,5 @@
-// main.cpp
 #include <iostream>
+#include <cmath>
 #include "sorting_algorithm.cpp"
 
 void PrintArray(int* array, int size) {
@@ -26,6 +26,28 @@ int main() {
     QuickSort(numbers, 0, size - 1, false);
 
     std::cout << "Array sorted in descending order:" << std::endl;
+    PrintArray(numbers, size);
+
+    // Calculate the square of each number
+    for (int i = 0; i < size; i++) {
+        numbers[i] *= numbers[i];
+    }
+
+    // Sort the array of squared values in ascending order
+    QuickSort(numbers, 0, size - 1, true);
+
+    std::cout << "Array sorted by squared values in ascending order:" << std::endl;
+    PrintArray(numbers, size);
+
+    // Calculate the square root of each squared value
+    for (int i = 0; i < size; i++) {
+        numbers[i] = static_cast<int>(std::sqrt(numbers[i]));
+    }
+
+    // Sort the array of square root values in ascending order
+    QuickSort(numbers, 0, size - 1, true);
+
+    std::cout << "Array sorted by square root of squared values in ascending order:" << std::endl;
     PrintArray(numbers, size);
 
     return 0;
